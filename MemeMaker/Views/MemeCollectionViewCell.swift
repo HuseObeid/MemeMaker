8deb85a42a92b13f70292554778aebbf01a35c7b
HuseObeid <obeidhu21@gmail.com>
2021-02-07 17:21:08 -0500

    [WIP] collectionViewCell
    added collectionView

---
+++ MemeMaker/Views/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMaker
+//
+//  Created by Hussein Obeid on 2021-02-07.
+//  Copyright © 2021 Adrian Domanico. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeCollectionImageView: UIImageView!
+    
+}
